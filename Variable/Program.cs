a7c293b29c8150cacc4031480a7600d71e9648d1
edase <edase@LAPTOP-UBLAETF7>
2026-02-01 22:43:49 +0300

    Add project files.

---
+++ Variable/Program.cs
@@ -0,0 +1,30 @@
+﻿// type safe language: degiskenin ismi ve tip bilgisini vermek zorundasın.
+
+String name = "Ahmet";
+Console.WriteLine("Merhaba "+name+" nasılsın?");
+
+//Tip güvenliği 
+// Derleme zamanı kontrolleri ile pek çok hatayı erken yakalar.
+// Dönüştürmeler, genericler nullable tür anotasyonları ve pattern matching ile güvenliği arttırır.
+
+Console.WriteLine("Lütfen adınızı giriniz: ");
+var ad = Console.ReadLine();
+Console.WriteLine("Merhaba " + ad);
+Console.WriteLine(ad?.GetType());
+
+//String yerine var da kullanılabilir dinamik bir atama yapmak istersen.
+
+
+//Nullcheck
+int? age = 22;
+var nam = "Ali";
+
+Console.WriteLine("age değişken tipi: " + age?.GetType()) ;
+Console.WriteLine("nam değişken tipi: " +nam.GetType()) ;
+
+string? nullable = null;  //nullable string
+Console.WriteLine("Büyük harf: " + nullable?.ToUpper());
+Console.WriteLine("Kullanıcı Adı " + (nullable ?? "Bilinmiyor"));
+Console.WriteLine("Yaş: " + (age ?? 0));
+
+Console.ReadKey();
